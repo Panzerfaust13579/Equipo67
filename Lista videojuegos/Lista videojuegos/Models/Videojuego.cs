55f1309f7dd5db32a0d00bda9de9b345de337be6
PedroOsorioo <osorioemiliano66@gmail.com>
2026-09-01 19:30:50 -0600

    Modelo
    Se crea el modelo para el videojuego

---
+++ Lista videojuegos/Lista videojuegos/Models/Videojuego.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista_videojuegos.Models
+{
+    internal class Videojuego
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string Categoria { get; set; } = string.Empty;
+        public string ImagenUrl { get; set; } = string.Empty;
+    }
+}
